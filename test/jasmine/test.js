a634153964151acf40bc67829a8461a93920bf0e
Curtis Cummings <curtis@curtiscummings.me>
2015-04-12 01:08:56 -0400

    Adding Jasmine 2.x adapter

--- test/jasmine/test.js
+++ test/jasmine/test.js
@@ -1,27 +1,20 @@
 describe('Test module', function () {
 
 	describe('It does something', function () {
-		it('Test ran!', function () {
-			expect(true).toBeTruthy();
+		xit('Skipped test', function() {
+			expect(false).toBeTruthy();
 		});
 
-		xit('Skipped test', function() {
+		it('Fails', function() {
 			expect(false).toBeTruthy();
 		});
 	});
 
 	describe('Some other suite', function() {
 		describe('Nested suite', function() {
-			it('Does something async', function(done) {
-				setTimeout(function() {
-					expect(true).toBeTruthy();
-					done();
-				}, 100);
+			it('Test ran!', function () {
+				expect(true).toBeTruthy();
 			});
-		});
-
-		it('Fails', function() {
-			expect(false).toBeTruthy();
 		});
 	});
 
